cf97c4fcc77dc093bd08a662f69f0652d2877b4f
malverngroup <jamesowillis1999@gmail.com>
2014-01-05 16:06:41 +0000

    Simple Message

--- Test/src/me/malverngroup/Test/Test.java
+++ Test/src/me/malverngroup/Test/Test.java
@@ -2,50 +2,45 @@ package me.malverngroup.Test;
 
 import java.util.logging.Logger;
 
+import me.malverngroup.Test.Test;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Test extends JavaPlugin
-{
+public class Test extends JavaPlugin{
 	public final Logger logger = Logger.getLogger("Minecraft");
 	public static Test plugin;
-	public static String args1;
-	
-	@Override
-	public void onDisable()
-	{
-		PluginDescriptionFile pdfFile = this.getDescription();
-		this.logger.info(pdfFile.getName() + " Has Been Disabled!");
-	}
-	@Override
-	public void onEnable()
-	{
-		PluginDescriptionFile pdfFile = this.getDescription();
-		this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() +" Has Been Enabled!");
-	}
+	public static String args2;
+
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
 	{
 		Player player = (Player) sender;
-		if(commandLabel.equalsIgnoreCase("helloworld"))
+		if(commandLabel.equalsIgnoreCase("msg"))
 		{
-			if(args.length == 0)
+			if(sender instanceof Player)
 			{
-				player.sendMessage(ChatColor.GREEN + "Hello World!");
-			}
-			else if (args.length == 1)
-			{
-				args1 = args[0];
-				player.sendMessage(ChatColor.RED + args1 + ChatColor.GREEN + " Hello Der!");
+				if(args.length == 0)
+				{	
+					player.sendMessage(ChatColor.GREEN + "Type /msg <name> <message>");
+				}
+				else
+				{
+			
+					args2 = args[1];
+					Player targetPlayer = player.getServer().getPlayer(args[0]);
+					targetPlayer.sendMessage(ChatColor.GREEN + "[" + player.getDisplayName() + "]" + " ---> " + args2);
+					
+				}
 			}
 			else
 			{
-				player.sendMessage(ChatColor.RED + "Too many Arguments!");
+				player.sendMessage(ChatColor.RED + "Sorry you needs to be player!");
 			}
 		}
 		return false;
 	}
+	
 }
